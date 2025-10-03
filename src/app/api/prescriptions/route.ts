5a4471a99fbb86e403a3bee81c3cf556ed7cbc1f
Joshypeace <joshuachilapondwa2@gmail.com>
2025-10-03 13:53:54 +0200

    added proper Prisma Calling in prescription route

--- src/app/api/prescriptions/route.ts
+++ src/app/api/prescriptions/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
-import { prisma } from '@/lib/prisma'
-import { Prisma, PrescriptionStatus } from '@prisma/client'
+import { prisma, Prisma } from '@/lib/prisma'
+import { PrescriptionStatus } from '@prisma/client'
 
 // GET all prescriptions with filtering
 export async function GET(request: Request) {
